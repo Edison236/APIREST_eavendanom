ece38fa3afc13a9b820f150c9bd253708b525882
Edison236 <edisonavendano1@gmail.com>
2024-11-24 23:25:46 -0500

    Creacion de mi primera API

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+from databases.db import db, init_db
+from models.guarderia import Guarderia
+from controllers.controller_animales import guarderia_bp
+
+import os
+
+load_dotenv()
+app = Flask(__name__, template_folder="views")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+init_db(app)
+app.register_blueprint(guarderia_bp)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':    
+    app.run(debug=True)
+    
