ba59f3ed11bf18cd90a191737effce81b351cd11
Edison236 <edisonavendano1@gmail.com>
2024-11-26 20:50:51 -0500

    Se hace un ajuste para mirarlo en el despliegue y que no genere errores

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ import os
 load_dotenv()
 app = Flask(__name__, template_folder="views")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db.init_app(app)
-init_db(app)
-app.register_blueprint(guarderia_bp)
+# db.init_app(app)
+# init_db(app)
+# app.register_blueprint(guarderia_bp)
 
 
 @app.route("/")
