bf47b8d39de90993cec11e678d5e5d0cc8988e6e
Edison236 <edisonavendano1@gmail.com>
2024-11-26 21:43:18 -0500

    Ajuste para la base de datos

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ load_dotenv()
 app = Flask(__name__, template_folder="views")
 
 # app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://root:KPcLrzsEngDJeXHahVjDdPwNKYcMzGEq@autorack.proxy.rlwy.net:25862/railway"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# db.init_app(app)
-# init_db(app)
-# app.register_blueprint(guarderia_bp)
+db.init_app(app)
+init_db(app)
+app.register_blueprint(guarderia_bp)
 
 
 @app.route("/")
